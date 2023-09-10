96232a9e3e484b69aa72cc059df6cc037e28b8c7
Max-Beier <max@maxbeier.dev>
2023-09-10 23:38:26 +0200

    Added vhs font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,12 @@
 
 <main class="flex justify-center items-center flex-col">
   <div class="h-screen w-screen flex justify-center items-center flex-col">
-    <h1 class="text-6xl">Vincent Reed</h1>
-    <h2 class="text-2xl">Head of Shredding Scythe Productions</h2>
+    <h1 class="vhs text-9xl">Vincent Reed</h1>
+    <h2 class="vhs text-4xl">Head of Shredding Scythe Productions</h2>
     <img class="w-8/12" src={BackgroundMedia} alt="Background media" />
   </div>
   <div class="h-screen w-screen flex justify-center items-center flex-col">
-    <h1 class="text-2xl">About Me</h1>
-    <h1 class="text-2xl">Contact</h1>
+    <h1 class="vhs text-4xl">About Me</h1>
+    <h1 class="vhs text-4xl">Contact</h1>
   </div>
 </main>
