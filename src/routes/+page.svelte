e5737106031442aed15c219186662ad4fdab26a7
Max-Beier <max@maxbeier.dev>
2023-09-11 22:17:23 +0200

    Added video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,29 @@
 <script lang="ts">
-  import DownArrow from "$lib/icons/Down Arrow Group.svg";
-  import BackgroundMedia from "$lib/media/background-test.gif";
+  import BackgroundMedia from "$lib/media/homevideo.mp4";
 </script>
 
 <svelte:head>
   <title>Vincent Reed</title>
 </svelte:head>
 
-<main class="flex flex-col items-center mt-5">
+<main class="flex flex-col items-center">
   <div
     class="w-full h-full flex flex-col justify-center items-center text-center"
   >
-    <div class="h-screen w-8/12 flex flex-col items-center">
-      <h1 class="vhs text-3xl">Vincent Reed</h1>
-      <h3 class="vhs text-lg">Head of Shredding Scythe Productions</h3>
+    <div class="h-screen w-screen flex flex-col items-center">
+      <div class="fixed w-full bg-black bg-opacity-50" />
 
-      <img
-        draggable="false"
-        class="vhs-box sm:w-96 md:w-8/12 mt-40 select-none pointer-events-none"
-        src={BackgroundMedia}
-        alt="Background media"
-      />
+      <!-- svelte-ignore a11y-media-has-caption -->
+      <video class="h-full w-full" src={BackgroundMedia} autoplay loop />
     </div>
 
     <div class="h-screen w-8/12 flex flex-col items-center">
-      <h2 class="vhs text-3xl mt-5">About Me</h2>
+      <h1 class="vhs text-3xl mt-5">Vincent Reed</h1>
+      <h3 class="vhs text-lg">Head of Shredding Scythe Productions</h3>
+      <h2 class="vhs text-3xl mt-28">About Me</h2>
       <p class="vhs text-lg">-</p>
       <h2 class="vhs text-3xl">Experience</h2>
-      <p class="vhs text-lg">-</p>
+      <p class="vhs text-lg" />
     </div>
   </div>
 </main>
