3c269e7c5778a2196d8d3e75e29c97e517d161e2
Max-Beier <max@maxbeier.dev>
2023-09-11 22:26:21 +0200

    Added title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,16 +10,17 @@
   <div
     class="w-full h-full flex flex-col justify-center items-center text-center"
   >
-    <div class="h-screen w-screen flex flex-col items-center">
-      <div class="fixed w-full bg-black bg-opacity-50" />
+    <div class="h-screen w-screen flex flex-col justify-center items-center">
+      <div class="absolute w-full">
+        <h1 class="vhs text-7xl mt-5">Vincent Reed</h1>
+        <h3 class="vhs text-4xl">Head of Shredding Scythe Productions</h3>
+      </div>
 
       <!-- svelte-ignore a11y-media-has-caption -->
       <video class="h-full w-full" src={Video} autoplay loop />
     </div>
 
     <div class="h-screen w-8/12 flex flex-col items-center">
-      <h1 class="vhs text-3xl mt-5">Vincent Reed</h1>
-      <h3 class="vhs text-lg">Head of Shredding Scythe Productions</h3>
       <h2 class="vhs text-3xl mt-28">About Me</h2>
       <p class="vhs text-lg">-</p>
       <h2 class="vhs text-3xl">Experience</h2>
